02e3bf030f8617afb6d5f5226f96a6cc98525de4
Colin Cross <ccross@android.com>
2026-08-25 14:59:28 -0700

    Implement sched_(get|set)(param|scheduler)
    Musl implemented these as returning -ENOSYS because POSIX specifies
    them as affecting the entire process, while the Linux syscall only
    affects a single thread.  Implement them anyways, as that's empircally
    what callers expect them to do.
    
    Test: concurrency_tests
    Change-Id: Ia981172d63197ce62a4c87db0801a76e24c3eb63

--- src/sched/sched_getparam.c
+++ src/sched/sched_getparam.c
@@ -1,8 +1,7 @@
 #include <sched.h>
-#include <errno.h>
 #include "syscall.h"
 
 int sched_getparam(pid_t pid, struct sched_param *param)
 {
-	return __syscall_ret(-ENOSYS);
+	return syscall(SYS_sched_getparam, pid, param);
 }
--- src/sched/sched_setscheduler.c
+++ src/sched/sched_setscheduler.c
@@ -1,8 +1,7 @@
 #include <sched.h>
-#include <errno.h>
 #include "syscall.h"
 
 int sched_setscheduler(pid_t pid, int sched, const struct sched_param *param)
 {
-	return __syscall_ret(-ENOSYS);
+	return syscall(SYS_sched_setscheduler, pid, sched, &param);
 }
